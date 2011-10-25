d8d3d920e3a6229f18d7b9814790957174136324
John Teague <jcteague@gmail.com>
2011-10-25 15:42:07 -0500

    test project

--- mvc-training/Controllers/HomeController.cs
+++ mvc-training/Controllers/HomeController.cs
@@ -7,11 +7,12 @@ namespace mvc_training.Controllers {
     public class HomeController : Controller {
         public ActionResult Index() {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
-
+            
             return View();
         }
 
         public ActionResult About() {
+           
             return View();
         }
     }
